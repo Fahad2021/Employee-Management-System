156d6ea8113657f6a1d305fc0773db1b6b6992b0
fahad2021 <fahadrahman810@gmail.com>
2022-06-28 12:04:51 +0600

    Adding Employee information API and add media

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MRDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -6,6 +6,7 @@ from django.http.response import JsonResponse
 
 from EmployeeApp.models import Department,Employee
 from EmployeeApp.serializers import DepartmentSerializers,EmployeeSerializers
+from django.core.files.storage import default_storage
 
 @csrf_exempt
 def departmentApi(request,id=0):
@@ -13,7 +14,6 @@ def departmentApi(request,id=0):
         departments=Department.objects.all()
         departments_serilizers=DepartmentSerializers(departments,many=True)
         return JsonResponse(departments_serilizers.data,safe=False)
-
     elif request.method=='POST':
         department_data=JSONParser().parse(request)
         departments_serilizer=DepartmentSerializers(data=department_data)
@@ -31,6 +31,39 @@ def departmentApi(request,id=0):
         return JsonResponse("Falied to update",safe=False)
 
     elif request.method=='DELETE':
-        departments=Department.objects.get(DepartmentId=id)
+        department=Department.objects.get(DepartmentId=id)
         department.delete()
         return JsonResponse("Delete Successfully",safe=False)
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method=='GET':
+        employees=Employee.objects.all()
+        employees_serilalizer=EmployeeSerializers(employees,many=True)
+        return JsonResponse(employees_serilalizer.data,safe=False)
+
+    elif request.method=='POST':
+        employee_data=JSONParser().parse(request)
+        employee_serilalizer=EmployeeSerializers(data=employee_data)
+        if employee_serilalizer.is_valid():
+            employee_serilalizer.save()
+            return JsonResponse("Add Employee successfully",safe=False)
+        return JsonResponse("Failed to save employee",safe=False)
+
+    elif request.method=='PUT':
+        employee_data=JSONParser().parse(request)
+        employee=Employee.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employees_serilizer=EmployeeSerializers(employee, data=employee_data)
+        if employees_serilizer.is_valid():
+            employees_serilizer.save()
+            return JsonResponse("Update successfully Employee",safe=False)
+        return JsonResponse("Falied to update Employee",safe=False)
+    elif request.method=='DELETE':
+        employee=Employee.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Delete sucessfully",safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+    file=request.FILES["PhotoUploaded"]
+    file_name=default_storage.save(file.name,file)
+    return JsonResponse(file_name,safe=False)
